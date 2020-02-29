26392fa725373e3a3079b498ca378c2a27431a73
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-02-28 22:50:38 -0800

    Add a function to handle face normal

--- src/hittable.rs
+++ src/hittable.rs
@@ -16,6 +16,14 @@ impl HitRecord {
             normal: Array1::zeros(3),
         }
     }
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Array1<f64>) {
+        let front_face = r.direction.dot(&outward_normal) < 0.;
+        self.normal = if front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+    }
 }
 
 pub trait Hittable {
@@ -31,24 +39,27 @@ impl Hittable for Sphere {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
         let oc = &r.origin - &self.center;
         let a = r.direction.dot(&r.direction);
-        let b = oc.dot(&r.direction);
+        let half_b = oc.dot(&r.direction);
         let c = oc.dot(&oc) - self.radius * self.radius;
-        let discriminant = b * b - a * c;
+        let discriminant = half_b * half_b - a * c;
 
         if discriminant > 0. {
-            let temp = (-b - discriminant.sqrt()) / a;
+            let root = discriminant.sqrt();
+            let temp = (-half_b - root) / a;
             if temp < t_max && temp > t_min {
                 rec.t = temp;
                 rec.p = r.point_at_parameter(rec.t);
-                rec.normal = (&rec.p - &self.center) / self.radius;
+                let outward_normal = (&rec.p - &self.center) / self.radius;
+                rec.set_face_normal(r, outward_normal);
                 return true;
             }
 
-            let temp = (-b + discriminant.sqrt()) / a;
+            let temp = (-half_b + root) / a;
             if temp < t_max && temp > t_min {
                 rec.t = temp;
                 rec.p = r.point_at_parameter(rec.t);
-                rec.normal = (&rec.p - &self.center) / self.radius;
+                let outward_normal = (&rec.p - &self.center) / self.radius;
+                rec.set_face_normal(r, outward_normal);
                 return true;
             }
         }
