acd492ee7a1d71b2576ee9a0fe38b970fa90b2fb
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-02-29 19:07:24 -0800

    Simplify HittableList hit function

--- src/hittable_list.rs
+++ src/hittable_list.rs
@@ -7,13 +7,12 @@ pub struct HittableList<T: Hittable> {
 
 impl<T: Hittable> Hittable for HittableList<T> {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
-        let mut temp_rec = rec;
         let mut hit_anything = false;
         let mut closest_so_far = t_max;
         for elem in self.list.iter() {
-            if elem.hit(r, t_min, closest_so_far, temp_rec) {
+            if elem.hit(r, t_min, closest_so_far, rec) {
                 hit_anything = true;
-                closest_so_far = temp_rec.t;
+                closest_so_far = rec.t;
             }
         }
 
