35bab8159ea3be4c39a004ec15ad80137c9ac3c2
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-03-02 23:42:40 -0800

    Add cli option to store to a file

---
+++ src/image.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+
+pub fn write_to_file(path: &Path, buffer: &Vec<u32>, width: usize, height: usize) {
+    let file = File::create(path).unwrap();
+    let w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, width as u32, height as u32);
+    encoder.set_color(png::ColorType::RGB);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+
+    let data = buffer
+        .iter()
+        .flat_map(|x| {
+            let mut m: Vec<u8> = Vec::new();
+            m.push((x & (2u32.pow(8) - 1)) as u8);
+            m.push((x & (2u32.pow(16) - 1) >> 8) as u8);
+            m.push((x & (2u32.pow(24) - 1) >> 16) as u8);
+            m
+        })
+        .collect::<Vec<u8>>();
+
+    writer.write_image_data(&data).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+mod image;
 mod window;
+use crate::image::write_to_file;
 use crate::window::run_window;
 use raytracer::run;
 use structopt::StructOpt;
@@ -15,11 +17,19 @@ struct Cli {
     /// Display the result in a floating window
     #[structopt(short = "w", long = "window")]
     window: bool,
+    /// Store the resulting image to a file
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    file: Option<std::path::PathBuf>,
 }
 
 fn main() {
     let args = Cli::from_args();
     let buffer = run(args.width, args.height);
+
+    if let Some(file) = args.file {
+        write_to_file(&file, &buffer, args.width, args.height);
+    }
+
     if args.window {
         if let Err(e) = run_window(&buffer, args.width, args.height) {
             panic!("Whops: {:?}", e);
